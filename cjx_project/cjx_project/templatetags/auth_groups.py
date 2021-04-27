160647ff18bc6b4899b91494fbe2d7d4dcb2efbf
cnam0203 <cnam0303@gmail.cim>
2021-04-27 15:03:08 +0700

    update authentication

--- cjx_project/cjx_project/templatetags/auth_groups.py
+++ cjx_project/cjx_project/templatetags/auth_groups.py
@@ -6,6 +6,6 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name): 
-    if user.groups.filter(name = group_name).exists():
+    if user.groups.filter(name = group_name).exists() or user.is_superuser:
         return True 
     return False
