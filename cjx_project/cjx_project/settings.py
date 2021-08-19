1fefaa5d3ccee5e6e48e2995a2b65b5df66fab72
chấn nam <47789251+cnam0203@users.noreply.github.com>
2021-08-19 13:05:10 +0700

    Update settings.py

--- cjx_project/cjx_project/settings.py
+++ cjx_project/cjx_project/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'cjx_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv("DB_ENGINE"),
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT")
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     },
 }
 
